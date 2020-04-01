a912034beda3021d5852659bf8c068833f2b5028
dukeTheLion <savio_gm@icloud.com>
2020-04-01 07:51:10 -0300

    MFX-01 MenuBar

---
+++ src/gui/MainViewControler.java
@@ -0,0 +1,52 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewControler implements Initializable {
+
+    @FXML
+    private MenuItem menuNewEmployee;
+    @FXML
+    private MenuItem menuNewDepartment;
+    @FXML
+    private MenuItem menuDeleteEmployee;
+    @FXML
+    private MenuItem menuDeleteDepartment;
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuNewEmployeeAction(){
+        System.out.println("Action New Employee");
+    }
+
+    @FXML
+    public void onMenuNewDepartmentAction(){
+        System.out.println("Action New Department");
+    }
+
+    @FXML
+    public void onMenuDeleteEmployeeAction(){
+        System.out.println("Action Delete Employee");
+    }
+
+    @FXML
+    public void onMenuDeleteDepartmentAction(){
+        System.out.println("Action Delete Department");
+    }
+
+    @FXML
+    public void onMenuHelpAboutAction(){
+        System.out.println("Action About");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
